e4e2e06009a9b975ddc2eb3d8560daff83378be3
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-10 18:22:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,79 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	if(arr.length<=1){
-		return 0;
-	}
-	let totalCost=0;
-	while(arr.length>1){
-		arr.sort((a,b)=>a-b);
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
 
-		let first = arr.shift();
-		let second = arr.shift();
+    insert(val) {
+        this.heap.push(val);
+        this._heapifyUp();
+    }
 
-		let cost=first+second;
-		totalCost=totalCost+cost;
-	}
-	return totalCost;
-  
+    extractMin() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this._heapifyDown();
+        return min;
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    _heapifyUp() {
+        let i = this.heap.length - 1;
+        while (i > 0) {
+            let parent = Math.floor((i - 1) / 2);
+            if (this.heap[i] < this.heap[parent]) {
+                [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
+                i = parent;
+            } else break;
+        }
+    }
+
+    _heapifyDown() {
+        let i = 0;
+        while (true) {
+            let left = 2 * i + 1;
+            let right = 2 * i + 2;
+            let smallest = i;
+
+            if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+                smallest = left;
+            }
+
+            if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+                smallest = right;
+            }
+
+            if (smallest !== i) {
+                [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
+                i = smallest;
+            } else break;
+        }
+    }
 }
-console.log(mincost([4, 3, 2, 6]));
-console.log(mincost([1, 2, 3, 4, 5]));
-module.exports=mincost;
+
+function mincost(arr) {
+    if (arr.length <= 1) return 0;
+
+    const minHeap = new MinHeap();
+    for (let rope of arr) {
+        minHeap.insert(rope);
+    }
+
+    let totalCost = 0;
+    while (minHeap.size() > 1) {
+        let first = minHeap.extractMin();
+        let second = minHeap.extractMin();
+        let cost = first + second;
+        totalCost += cost;
+        minHeap.insert(cost);
+    }
+
+    return totalCost;
+}
+console.log(mincost([4, 3, 2, 6])); // Output: 29
+console.log(mincost([1, 2, 3, 4, 5])); // Output: 33
+
+module.exports = mincost;
